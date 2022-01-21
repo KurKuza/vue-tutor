f975772512543122bcd643cfbcdfa2431d1be476
KurKuza <47026225+KurKuza@users.noreply.github.com>
2022-01-21 11:43:47 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app">
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Javacript", body: "Описание" },
+        { id: 2, title: "Javacript 2", body: "Описание 2" },
+        { id: 3, title: "Javacript 3", body: "Описание 3" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post, second, third) {
+      this.posts.push(post);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <form @submit.prevent>
+    <h4>Cоздание поста</h4>
+    <input
+      v-bind:value="post.title"
+      @input="post.title = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-bind:value="post.body"
+      @input="post.body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background-color: none;
+  cursor: pointer;
+  color: teal;
+  border: 1px solid teal;
+}
+input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  outline: none;
+  margin-top: 10px;
+}
+</style>
